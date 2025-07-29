3bdaffe6f296fb490b7fa3e5cb167b0fde628c65
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2025-07-29 16:07:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '40px' }}>
+      <h1>Hello from React via CloudFront!</h1>
+      <p>This static app is deployed to S3 and served through CloudFront.</p>
+    </div>
+  );
+}
+
+export default App;
 
