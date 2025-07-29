0a04adbad3f643c261e116c01bf503a2d6efc7cc
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2025-07-29 17:47:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div style={{ textAlign: 'center', marginTop: '40px' }}>
       <h1>Hello from React via CloudFront!</h1>
       <p>This static app is deployed to S3 and served through CloudFront.</p>
+      <h2>By Shraddha</h2>
     </div>
   );
 }
