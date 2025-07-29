435fc45950976087762a5e31df90a4ffd99af46c
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2025-07-29 18:24:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <h1>Hello from React via CloudFront!</h1>
-      <p>This static app is deployed to <strong>S3</strong> and served through <strong>CloudFront</strong>.</p>
-      <h2>By Shraddha</h2>
+     <h1>Hello from React via CloudFront!</h1>
+     <p>This static app is deployed to <strong style="color: #ff6f61;">S3</strong> and served through <strong style="color: #ff6f61f;">CloudFront</strong>.</p>
+     <h2>By Shraddha</h2>
     </div>
   );
 }
